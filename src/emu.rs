2049c49eb7681d7da96f57d024680bf616db3c74
Anciety <anciety@starcross.cn>
2021-07-16 18:56:33 +0800

    implement add inst for x64 jit

--- src/emu.rs
+++ src/emu.rs
@@ -1,18 +1,52 @@
-use dynasmrt::{AssemblyOffset, ExecutableBuffer};
+use std::{convert::TryInto, num::TryFromIntError};
+
+use dynasmrt::{relocations::Relocation, Assembler};
 use memmap2::Mmap;
 
 #[cfg(target_arch = "x86_64")]
 mod x86_64;
 #[cfg(target_arch = "x86_64")]
-pub use x86_64::{PcodeCacheOnlyTranslator};
+pub use x86_64::PcodeCacheOnlyTranslator;
 
-use crate::{PcodeOp, Varnode};
+use crate::{Address, OpCode, PcodeOp, Varnode};
 
 /// single pcode translate
-pub(crate) trait PcodeTranslator : Default {
-    fn int_add(&mut self, lhs: &dyn Varnode, rhs: &dyn Varnode, out: &dyn Varnode);
+pub trait PcodeTranslator: std::fmt::Debug {
+    type Reloc: Relocation;
+    type Mem: Memory;
 
-    fn translate_pcode(&mut self, pcode: &dyn PcodeOp) -> Result<(AssemblyOffset, ExecutableBuffer)> {
+    fn int_add(
+        &mut self,
+        ops: &mut Assembler<Self::Reloc>,
+        mem: &Self::Mem,
+        inputs: &[&dyn Varnode],
+        out: &dyn Varnode,
+    ) -> Result<()>;
+    // todo: other operations..
+
+    fn translate_pcode(
+        &mut self,
+        ops: &mut Assembler<Self::Reloc>,
+        mem: &Self::Mem,
+        pcode: &dyn PcodeOp,
+    ) -> Result<()> {
+        use OpCode::*;
+
+        match pcode.opcode() {
+            IntAdd => {
+                let inputs = pcode.inputs();
+                let output = pcode.output();
+
+                // without output, IntAdd takes no effect then
+                if output.is_none() {
+                    return Ok(());
+                }
+
+                let output = output.unwrap();
+                self.int_add(ops, mem, &inputs, output)?;
+            }
+            _ => todo!("other opcodes"),
+        }
         todo!()
     }
 }
@@ -22,33 +56,50 @@ pub enum EmuError {
     /// Cache-only translator requires every address is in the cache. If not, this error indicates
     /// that it is unable to continue execution.
     NotInCache(usize),
+    /// This address cannot be translated into target memory addressing.
+    /// Address is represented by (space, offset)
+    UnknownAddr((String, usize)),
+    /// Host addressing is not enough for target machine.
+    NotEnoughAddressing(TryFromIntError),
 }
+
+// TODO: implement Error for EmuError
 
 pub type Result<T> = std::result::Result<T, EmuError>;
 
-pub trait Memory : std::fmt::Debug {
+pub trait Memory: std::fmt::Debug {
+    /// This memory's actual addressing
+    type MemAddr;
 
+    /// Translate the pcode addr into this memory's addressing
+    fn translate(&self, addr: &dyn Address) -> Result<Self::MemAddr>;
 }
 
 /// Emulator Memory
-/// `BlockTranslator` is responisble for translating a single block into executable pcode
-pub trait BlockTranslator<Mem: Memory> : std::fmt::Debug {
+/// `BlockTranslator` is responsible for translating a single block into executable pcode
+pub trait BlockTranslator<Mem: Memory>: std::fmt::Debug {
     fn translate(&mut self, mem: &mut Mem, addr: usize) -> Result<*const u8>;
 }
 
 #[derive(Debug)]
-pub struct Emulator<Trans, Mem> where Trans: BlockTranslator<Mem>, Mem: Memory {
+pub struct Emulator<Trans, Mem>
+where
+    Trans: BlockTranslator<Mem>,
+    Mem: Memory,
+{
     /// translator used to translate the blocks
     trans: Trans,
     /// internal memory implementation
-    mem: Mem
+    mem: Mem,
 }
 
-impl<Trans, Mem> Emulator<Trans, Mem> where Trans: BlockTranslator<Mem>, Mem: Memory {
+impl<Trans, Mem> Emulator<Trans, Mem>
+where
+    Trans: BlockTranslator<Mem>,
+    Mem: Memory,
+{
     pub fn new(trans: Trans, mem: Mem) -> Self {
-        Self {
-            trans, mem
-        }
+        Self { trans, mem }
     }
 
     /// Run until the end of the program
@@ -56,9 +107,7 @@ impl<Trans, Mem> Emulator<Trans, Mem> where Trans: BlockTranslator<Mem>, Mem: Me
         // translate the fall back block then call it.
         // Note that only the first block should be reside in a call, as it can be returned.
         let entry_block = self.trans.translate(&mut self.mem, entry)?;
-        let entry_func : extern "C" fn () = unsafe {
-            std::mem::transmute(entry_block)
-        };
+        let entry_func: extern "C" fn() = unsafe { std::mem::transmute(entry_block) };
         entry_func();
         Ok(())
     }
@@ -68,7 +117,33 @@ impl<Trans, Mem> Emulator<Trans, Mem> where Trans: BlockTranslator<Mem>, Mem: Me
 #[derive(Debug)]
 pub struct MemMappedMemory {
     /// vec of (begin, size). All these memories are actually mapped.
-    regions: Vec<Mmap>
+    regions: Vec<Mmap>,
+    /// the base of the register
+    reg_base: usize,
 }
 
-impl Memory for MemMappedMemory {}
+impl MemMappedMemory {
+    pub fn new(reg_base: usize) -> Self {
+        Self {
+            reg_base,
+            regions: vec![],
+        }
+    }
+}
+
+impl Memory for MemMappedMemory {
+    type MemAddr = usize;
+
+    fn translate(&self, addr: &dyn Address) -> Result<Self::MemAddr> {
+        let offset: usize = addr
+            .offset()
+            .try_into()
+            .map_err(|e| EmuError::NotEnoughAddressing(e))?;
+
+        match addr.space().as_str() {
+            "register" => Ok(offset + self.reg_base),
+            "ram" => Ok(offset),
+            _ => Err(EmuError::UnknownAddr((addr.space(), offset))),
+        }
+    }
+}
--- src/emu/x86_64.rs
+++ src/emu/x86_64.rs
@@ -1,50 +1,153 @@
-use std::collections::{BTreeMap, HashMap};
-use dynasmrt::{x64, DynasmApi, AssemblyOffset, ExecutableBuffer};
+use super::{BlockTranslator, MemMappedMemory, PcodeTranslator, Result};
+use crate::{emu::Memory, PcodeOp};
 use dynasm::dynasm;
-use crate::PcodeOp;
-use super::{MemMappedMemory, BlockTranslator, EmuError, Result};
+use dynasmrt::{
+    x64::{self, X64Relocation},
+    AssemblyOffset, DynasmApi, ExecutableBuffer,
+};
+use std::collections::{BTreeMap, HashMap};
+
+/// Jitting pcode translator. Translate pcode by jitting it into x86 asm.
+#[derive(Debug)]
+pub struct X64JitPcodeTranslator {
+    /// register base address
+    reg_base: usize,
+}
+
+impl PcodeTranslator for X64JitPcodeTranslator {
+    type Mem = MemMappedMemory;
+    type Reloc = X64Relocation;
+
+    fn int_add(
+        &mut self,
+        ops: &mut dynasmrt::Assembler<Self::Reloc>,
+        mem: &MemMappedMemory,
+        inputs: &[&dyn crate::Varnode],
+        out: &dyn crate::Varnode,
+    ) -> Result<()> {
+        dynasm!(ops
+            ; xor rax, rax // rax is the accumulation value
+        );
+
+        for input in inputs {
+            if &input.addr().space() == "const" {
+                // add a const
+                let value = input.addr().offset();
+                dynasm!(ops
+                    ; add rax, value as _
+                );
+            } else {
+                let input_addr = mem.translate(input.addr())?;
+                let size = input.size();
+                match size {
+                    1 => {
+                        dynasm!(ops
+                            ; xor rdx, rdx
+                            ; mov dl, BYTE [input_addr as _]
+                            ; add rax, rdx
+                        );
+                    }
+                    2 => {
+                        dynasm!(ops
+                            ; xor rdx, rdx
+                            ; mov dx, WORD [input_addr as _]
+                            ; add rax, rdx
+                        );
+                    }
+                    4 => {
+                        dynasm!(ops
+                            ; xor rdx, rdx
+                            ; mov edx, DWORD [input_addr as _]
+                            ; add rax, rdx
+                        );
+                    }
+                    8 => {
+                        dynasm!(ops
+                            ; add rax, QWORD [input_addr as _]
+                        );
+                    }
+                    _ => unreachable!(),
+                }
+            }
+        }
+
+        let out_addr = mem.translate(out.addr())?;
+        let size = out.size();
+
+        match size {
+            1 => {
+                dynasm!(ops
+                    ; mov BYTE [out_addr as _], al
+                );
+            }
+            2 => {
+                dynasm!(ops
+                    ; mov WORD [out_addr as _], ax
+                );
+            }
+            4 => {
+                dynasm!(ops
+                    ; mov DWORD [out_addr as _], eax
+                );
+            }
+            8 => {
+                dynasm!(ops
+                    ; mov QWORD [out_addr as _], rax
+                );
+            }
+            _ => unreachable!(),
+        }
+
+        Ok(())
+    }
+}
 
 /// Pcode cache only block translator that does not translate anything not in the cache.
 /// In other words, no actual translation engine is included.
-#[derive(Debug, Default)]
-pub struct PcodeCacheOnlyTranslator<'a> {
+#[derive(Debug)]
+pub struct PcodeCacheOnlyTranslator<'a, PcodeTrans: PcodeTranslator> {
     /// addr to index into `pcode_cache`
     cache: BTreeMap<usize, &'a dyn PcodeOp>,
     block_cache: HashMap<usize, (AssemblyOffset, ExecutableBuffer)>,
+    pcode_trans: PcodeTrans,
 }
 
-impl<'a> PcodeCacheOnlyTranslator<'a> {
-    fn from_cache(cache: BTreeMap<usize, &'a dyn PcodeOp>) -> Self {
+impl<'a, PcodeTrans: PcodeTranslator> PcodeCacheOnlyTranslator<'a, PcodeTrans> {
+    pub fn from_cache(cache: BTreeMap<usize, &'a dyn PcodeOp>, pcode_trans: PcodeTrans) -> Self {
         Self {
             cache,
-            block_cache: HashMap::default()
+            block_cache: HashMap::default(),
+            pcode_trans,
         }
     }
 
-    fn add_pcode(&mut self, pcode: &'a dyn PcodeOp, addr: usize) {
+    pub fn add_pcode(&mut self, pcode: &'a dyn PcodeOp, addr: usize) {
         self.cache.insert(addr, pcode);
     }
 }
 
-impl<'a> BlockTranslator<MemMappedMemory> for PcodeCacheOnlyTranslator<'a> {
-    fn translate(&mut self, _mem: &mut MemMappedMemory, addr: usize) -> Result<*const u8> {
+impl<'a, PcodeTrans: PcodeTranslator<Reloc = X64Relocation, Mem = MemMappedMemory>>
+    BlockTranslator<MemMappedMemory> for PcodeCacheOnlyTranslator<'a, PcodeTrans>
+{
+    fn translate(&mut self, mem: &mut MemMappedMemory, addr: usize) -> Result<*const u8> {
+        let cache_op_iter = self.cache.iter().skip_while(|c| *c.0 != addr);
 
-        let cached_op = match self.cache.get(&addr) {
-            Some(op) => op,
-            None => return Err(EmuError::NotInCache(addr))
-        };
+        let mut ops = x64::Assembler::new().unwrap();
 
-        use crate::OpCode::*;
+        dynasm!(ops
+            ; .arch x64
+        );
+        let offset = ops.offset();
 
-        let ops = x64::Assembler::new().unwrap();
-
-        match cached_op.opcode() {
-            IntAdd => {
-                todo!("do the actual translation")
-            },
-            _ => {}
+        for (_, pcode) in cache_op_iter {
+            self.pcode_trans.translate_pcode(&mut ops, mem, *pcode)?;
         }
 
-        todo!("implement the op translation")
+        let block_result = ops.finalize().unwrap();
+        let buf = block_result.ptr(offset);
+
+        self.block_cache.insert(addr, (offset, block_result));
+
+        Ok(buf)
     }
 }
