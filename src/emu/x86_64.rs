31e30a148e005fa4a77aa79c7d63ec01461e5385
Anciety <anciety@starcross.cn>
2021-07-22 12:37:06 +0800

    implement some of the int operations

--- src/emu/x86_64.rs
+++ src/emu/x86_64.rs
@@ -7,32 +7,32 @@ use dynasmrt::{
 };
 use std::collections::{BTreeMap, HashMap};
 
-macro_rules! init_reg_rdx {
+macro_rules! init_reg_rcx {
     ($ops:ident, $mem:ident, $input_varnode:ident) => {
         let input_addr = $mem.translate_addr($input_varnode.addr())?;
         let size = $input_varnode.size();
         match size {
             1 => {
                 dynasm!($ops
-                    ; xor rdx, rdx
-                    ; mov dl, BYTE [input_addr as _]
+                    ; xor rcx, rcx
+                    ; mov cl, BYTE [input_addr as _]
                 );
             }
             2 => {
                 dynasm!($ops
-                    ; xor rdx, rdx
-                    ; mov dx, WORD [input_addr as _]
+                    ; xor rcx, rcx
+                    ; mov cx, WORD [input_addr as _]
                 );
             }
             4 => {
                 dynasm!($ops
-                    ; xor rdx, rdx
-                    ; mov edx, DWORD [input_addr as _]
+                    ; xor rcx, rcx
+                    ; mov ecx, DWORD [input_addr as _]
                 );
             }
             8 => {
                 dynasm!($ops
-                    ; mov rdx, QWORD [input_addr as _]
+                    ; mov rcx, QWORD [input_addr as _]
                 );
             },
             _ => unreachable!()
@@ -81,8 +81,120 @@ macro_rules! ensure_output {
     ($out:ident) => {
         let $out = match $out {
             Some(out) => out,
-            None => return Ok(())
+            None => return Ok(()),
         };
+    };
+}
+
+/// This macro is used to simplify the implement of various arithmetic
+/// operations which looks quite the same.
+///
+/// usage: `impl_int_arith_jit_pcode(opname [final assembly])
+///
+/// The final assembly part is the actual opertaion in x86 assembly which
+/// the accumulator value is in rax and the actual value is in rcx.
+///
+/// An example of int_add implement looks like the following code:
+///
+/// ```ignore
+/// impl_int__arith_jit_pcode!(int_add [add rax, rcx]);
+/// ```
+macro_rules! impl_int_arith_jit_pcode {
+    ($opname:ident [$op:tt $dst_reg:tt, $src_reg:tt]) => {
+        fn $opname(
+            &mut self,
+            ops: &mut dynasmrt::Assembler<Self::Reloc>,
+            mem: &mut MemMappedMemory,
+            inputs: &[&dyn crate::Varnode],
+            out: Option<&dyn crate::Varnode>
+        ) -> Result<()> {
+            ensure_output!(out);
+            dynasm!(ops
+                ; xor rax, rax
+            );
+            for input in inputs {
+                if &input.addr().space() == "const" {
+                    let value = input.addr().offset();
+                    dynasm!(ops
+                        ; $op $dst_reg, value as _
+                    );
+                } else {
+                    init_reg_rcx!(ops, mem, input);
+                    dynasm!(ops
+                        ; $op $dst_reg, $src_reg
+                    );
+                }
+            }
+            fini_reg_rax!(ops, mem, out);
+            Ok(())
+        }
+    };
+    ($opname:ident [$op:tt $src_reg:tt]) => {
+        fn $opname(
+            &mut self,
+            ops: &mut dynasmrt::Assembler<Self::Reloc>,
+            mem: &mut MemMappedMemory,
+            inputs: &[&dyn crate::Varnode],
+            out: Option<&dyn crate::Varnode>
+        ) -> Result<()> {
+            ensure_output!(out);
+            dynasm!(ops
+                ; xor rax, rax
+            );
+            for input in inputs {
+                if &input.addr().space() == "const" {
+                    let value = input.addr().offset();
+                    // many 1 operand operations don't support immediate
+                    // e.g: mul, div..
+                    dynasm!(ops
+                        ; mov $src_reg, value as _
+                        ; $op $src_reg
+                    );
+                } else {
+                    init_reg_rcx!(ops, mem, input);
+                    dynasm!(ops
+                        ; $op $src_reg
+                    );
+                }
+            }
+            fini_reg_rax!(ops, mem, out);
+            Ok(())
+        }
+    };
+    // used in remainder
+    ($opname:ident [$op:tt $src_reg:tt; $fini_op:tt $fini_dst_reg:tt, $fini_src_reg:tt]) => {
+        fn $opname(
+            &mut self,
+            ops: &mut dynasmrt::Assembler<Self::Reloc>,
+            mem: &mut MemMappedMemory,
+            inputs: &[&dyn crate::Varnode],
+            out: Option<&dyn crate::Varnode>
+        ) -> Result<()> {
+            ensure_output!(out);
+            dynasm!(ops
+                ; xor rax, rax
+            );
+            for input in inputs {
+                if &input.addr().space() == "const" {
+                    let value = input.addr().offset();
+                    // many 1 operand operations don't support immediate
+                    // e.g: mul, div..
+                    dynasm!(ops
+                        ; mov $src_reg, value as _
+                        ; $op $src_reg
+                        ; $fini_op $fini_dst_reg, $fini_src_reg
+                    );
+                } else {
+                    init_reg_rcx!(ops, mem, input);
+                    dynasm!(ops
+                        ; $op $src_reg
+                        ; $fini_op $fini_dst_reg, $fini_src_reg
+                    );
+                }
+            }
+            fini_reg_rax!(ops, mem, out);
+            Ok(())
+        }
     };
 }
 
@@ -90,34 +202,19 @@ impl PcodeTranslator for X64JitPcodeTranslator {
     type Mem = MemMappedMemory;
     type Reloc = X64Relocation;
 
-    fn int_add(
-        &mut self,
-        ops: &mut dynasmrt::Assembler<Self::Reloc>,
-        mem: &mut MemMappedMemory,
-        inputs: &[&dyn crate::Varnode],
-        out: Option<&dyn crate::Varnode>,
-    ) -> Result<()> {
-        ensure_output!(out);
-        dynasm!(ops
-            ; xor rax, rax // rax is the accumulation value
-        );
-        for input in inputs {
-            if &input.addr().space() == "const" {
-                // add a const
-                let value = input.addr().offset();
-                dynasm!(ops
-                    ; add rax, value as _
-                );
-            } else {
-                init_reg_rdx!(ops, mem, input);
-                dynasm!(ops
-                    ; add rax, rdx
-                );
-            }
-        }
-        fini_reg_rax!(ops, mem, out);
-        Ok(())
-    }
+    impl_int_arith_jit_pcode!(int_add [add rax, rcx]);
+    impl_int_arith_jit_pcode!(int_sub [sub rax, rcx]);
+    impl_int_arith_jit_pcode!(int_xor [xor rax, rcx]);
+    impl_int_arith_jit_pcode!(int_and [and rax, rcx]);
+    impl_int_arith_jit_pcode!(int_or [or rax, rcx]);
+    impl_int_arith_jit_pcode!(int_left [shl rax, cl]);
+    impl_int_arith_jit_pcode!(int_right [shr rax, cl]);
+    impl_int_arith_jit_pcode!(int_sright [sar rax, cl]);
+    impl_int_arith_jit_pcode!(int_mult [mul rcx]);
+    impl_int_arith_jit_pcode!(int_div [div rcx]);
+    impl_int_arith_jit_pcode!(int_rem[div rcx; mov rax, rdx]);
+    impl_int_arith_jit_pcode!(int_sdiv [idiv rcx]);
+    impl_int_arith_jit_pcode!(int_srem[idiv rcx; mov rax, rdx]);
 }
 
 /// Pcode cache only block translator that does not translate anything not in the cache.
